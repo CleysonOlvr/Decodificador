20755341f3b1c69b54bd8b9e61d86b64a206c89c
Cleyson Oliveira <cleyson.gc@gmail.com>
2023-01-19 21:58:07 -0300

    Fixing some bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 var textInput = document.querySelector("#decodificador-de-texto");
-var textOutput = document.querySelector("output-screen-2");
+var textOutput = document.querySelector("output-screen-1");
+
+function criptografar() {
+    var texto = textInput.value;
+
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="decodificador-de-texto">' + resultCripto + '</textarea>'+ '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>';
+}
+
+function descriptografar() {
+    var texto = textInput.value;
+
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultDescripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
